3016e96863a607ba17235833bbbb01fdb439f221
ersin buckley <ersinbuckley@gmail.com>
2015-10-13 11:57:50 +1300

    update frontend to use state based navigation + add registration view
    - add the ui.route dependency
    - add bootstrap css
    - add register view
    - break list/login features into there own state
    - improve style and UX by adding a navbar

--- client/js/app.js
+++ client/js/app.js
@@ -1,56 +1,115 @@
 (function (window, angular) {
 	'use strict';
-	angular.module('quoteBook', ['lbServices', 'ngRoute'])
-	.controller('mainCtrl', function ($scope, User) {
+	angular.module('quoteBook', ['lbServices', 'ui.router'])
+	.config(function ($stateProvider) {
+		$stateProvider.state('login', {
+			url:'/login',
+			controller: 'loginCtrl',
+			templateUrl: '/views/login.html'
+		});
+
+		$stateProvider.state('list', {
+			url:'/list',
+			templateUrl: '/views/list.html',
+			controller: 'listCtrl'
+		});
+
+		$stateProvider.state('register', {
+			url:'/register',
+			templateUrl: '/views/register.html',
+			controller: 'registerCtrl'
+		});
+	})
+	.controller('loginCtrl', function ($scope, User, $state) {
+		if (User.isAuthenticated()) {
+			$state.go('list');
+		}
+
+		$scope.credentials = {};
+
+			$scope.login = function () {
+				User.login($scope.credentials,
+					function (success) {
+						$scope.user = success.user;
+						$state.go('list');
+					}, function  (err) {
+						$scope.pendingLogin = false;
+						console.log('error logging in', err);
+					});
+			};
+	})
+	.controller('registerCtrl', function  ($scope, User, $state) {
+
+		if (User.isAuthenticated()) {
+			$state.go('list');
+		}
+
+		$scope.doRegister = function  () {
+			$scope.pending = true;
+
+			User.create($scope.credentials)
+			.$promise.then(function  (success) {
+				return User.login($scope.credentials).$promise;
+			})
+			.then(function  () {
+					$scope.pending = false;
+					$state.go('list')
+			}, function (err) {
+					$scope.pending = false;
+			});
+
+		};
+	})
+	.controller('listCtrl', function ($scope, User, $state) {
+
+		if (!User.isAuthenticated()) {
+			$state.go('login');
+		}
+
+		User.quotes({ id: User.getCurrentId() },
+			function (success) {
+				console.log('got quotes', success);
+				$scope.quotes = success;
+			}, function (err) {
+				console.log('error getting quotes', err);
+			});
+	})
+	.controller('mainCtrl', function ($scope, User, $state) {
+
+		if (User.isAuthenticated()) {
+			$state.go('list')
+		} else {
+			$state.go('login');
+		}
 
 		$scope.pendingLogin = false;
 		$scope.credentials = {};
 		$scope.quotes = [];
 
-		if (User.isAuthenticated()) {
-			User.get({id: User.getCurrentId()}
-			, function (success) {
-				$scope.user = success;
-			}, function (err) {
-				console.log("fatal error, couldn't get user from backend", err);
-			});
-		} else {
-			$scope.user = false;
-		}
-
-
-		$scope.logout = function () {
-			
-			User.logout({},
-			function  (success) {
-				console.log('successful logout', success);
-				$scope.user = false;
-			}, function  (err) {
-				console.log('fail logout', err);
-			})
-		}
-
-		$scope.login = function () {
-			User.login($scope.credentials,
-			function (success) {
-				$scope.user = success.user;
-				console.log('we logged in woot!', success);
-			}, function  (err) {
-				$scope.pendingLogin = false;
-				console.log('error logging in', err);
-			})
-		};
-
-		$scope.$watch('user', function (newTok, old) {
-			if (newTok) {
-				User.quotes({ id: $scope.user.id },
-				function (success) {
-					console.log('got quotes', success);
-					$scope.quotes = success;
+		$scope.$watch(function  () {
+			return User.isAuthenticated()
+		}, function (isAuthed) {
+			if (isAuthed) {
+				User.get({id: User.getCurrentId()}
+				, function (success) {
+					$scope.user = success;
 				}, function (err) {
-					console.log('error getting quotes', err);
+					console.log("fatal error, couldn't get user from backend", err);
 				});
+			} else {
+				$scope.user = false;
+				$state.go('login');
 			}
 		});
+
+		$scope.logout = function () {
+			User.logout({},
+				function  (success) {
+					$scope.user = false;
+					$state.go('login');
+				}, function  (err) {
+					console.log('fail logout', err);
+				})
+		}
 	});
 })(window, angular)
